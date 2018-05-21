bfeaa88855ed0850758b40a77072132d9a79b4ca
davidvhill <davehill75@gmail.com>
2018-05-21 13:22:56 -0500

    Version 1.0.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject nemo "1.0.0-SNAPSHOT"
+(defproject nemo "1.0.0"
   :description "Cassandra table HTTP Adapter"
   :url "http://github.com/usgs-eros/lcmap-nemo"
   :license {:name "Unlicense"
